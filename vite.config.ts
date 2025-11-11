88a3401659aa5e927b744866377a45b7668c5593
Deniza Bekova <Deniza.Bekova@getrealprice.com>
2025-11-12 02:37:37 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/kpop_yona/' : '/',
   plugins: [react()],
-})
-
+}))
